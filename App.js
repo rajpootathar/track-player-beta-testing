14ac68c82eb243aee17fb6abc5022beb8aa50a57
rajpootathar <rajpootathar@gmail.com>
2021-12-14 11:05:16 +0500

    track player example added

--- App.js
+++ App.js
@@ -1,111 +1,216 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
+  Image,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
+import Slider from '@react-native-community/slider';
+import TrackPlayer, {
+  Capability,
+  Event,
+  RepeatMode,
+  State,
+  usePlaybackState,
+  useProgress,
+  useTrackPlayerEvents,
+} from 'react-native-track-player';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// @ts-ignore
+import playlistData from './react/data/playlist.json';
+// @ts-ignore
+import localTrack from './react/resources/pure.m4a';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const setupIfNecessary = async () => {
+
+  await TrackPlayer.setupPlayer({});
+  await TrackPlayer.updateOptions({
+    stopWithApp: false,
+    capabilities: [
+      Capability.Play,
+      Capability.Pause,
+      Capability.SkipToNext,
+      Capability.SkipToPrevious,
+      Capability.Stop,
+    ],
+    compactCapabilities: [Capability.Play, Capability.Pause],
+  });
+
+  await TrackPlayer.add(playlistData);
+  await TrackPlayer.add({
+    url: localTrack,
+    title: 'Pure (Demo)',
+    artist: 'David Chavez',
+    artwork: 'https://i.scdn.co/image/e5c7b168be89098eb686e02152aaee9d3a24e5b6',
+    duration: 28,
+  });
+
+  TrackPlayer.setRepeatMode(RepeatMode.Queue);
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const togglePlayback = async playbackState => {
+  const currentTrack = await TrackPlayer.getCurrentTrack();
+  if (currentTrack == null) {
+    // TODO: Perhaps present an error or restart the playlist?
+  } else {
+    if (playbackState !== State.Playing) {
+      await TrackPlayer.play();
+    } else {
+      await TrackPlayer.pause();
+    }
+  }
+};
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const Player = () => {
+  const playbackState = usePlaybackState();
+  const progress = useProgress();
+
+  const [trackArtwork, setTrackArtwork] = useState();
+  const [trackTitle, setTrackTitle] = useState();
+  const [trackArtist, setTrackArtist] = useState();
+
+  useTrackPlayerEvents([Event.PlaybackTrackChanged], async event => {
+    if (
+      event.type === Event.PlaybackTrackChanged &&
+      event.nextTrack !== undefined
+    ) {
+      const track = await TrackPlayer.getTrack(event.nextTrack);
+      const {title, artist, artwork} = track || {};
+      setTrackTitle(title);
+      setTrackArtist(artist);
+      setTrackArtwork(artwork);
+    }
+  });
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.screenContainer}>
+      <StatusBar barStyle={'light-content'} />
+      <View style={styles.contentContainer}>
+        <View style={styles.topBarContainer}>
+          <TouchableWithoutFeedback>
+            <Text style={styles.queueButton}>Queue</Text>
+          </TouchableWithoutFeedback>
         </View>
-      </ScrollView>
+        <Image style={styles.artwork} source={{uri: `${trackArtwork}`}} />
+        <Text style={styles.titleText}>{trackTitle}</Text>
+        <Text style={styles.artistText}>{trackArtist}</Text>
+        <Slider
+          style={styles.progressContainer}
+          value={progress.position}
+          minimumValue={0}
+          maximumValue={progress.duration}
+          thumbTintColor="#FFD479"
+          minimumTrackTintColor="#FFD479"
+          maximumTrackTintColor="#FFFFFF"
+          onSlidingComplete={async value => {
+            await TrackPlayer.seekTo(value);
+          }}
+        />
+        <View style={styles.progressLabelContainer}>
+          <Text style={styles.progressLabelText}>
+            {new Date(progress.position * 1000).toISOString().substr(14, 5)}
+          </Text>
+          <Text style={styles.progressLabelText}>
+            {new Date((progress.duration - progress.position) * 1000)
+              .toISOString()
+              .substr(14, 5)}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.actionRowContainer}>
+        <TouchableWithoutFeedback onPress={() => TrackPlayer.skipToPrevious()}>
+          <Text style={styles.secondaryActionButton}>Prev</Text>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={() => togglePlayback(playbackState)}>
+          <Text style={styles.primaryActionButton}>
+            {playbackState === State.Playing ? 'Pause' : 'Play'}
+          </Text>
+        </TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={() => TrackPlayer.skipToNext()}>
+          <Text style={styles.secondaryActionButton}>Next</Text>
+        </TouchableWithoutFeedback>
+      </View>
     </SafeAreaView>
   );
 };
 
+const App = () => {
+  useEffect(() => {
+    setupIfNecessary();
+  }, []);
+  return <Player />;
+};
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  screenContainer: {
+    flex: 1,
+    backgroundColor: '#212121',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
+  topBarContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    justifyContent: 'flex-end',
+  },
+  queueButton: {
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: 'bold',
+    color: '#FFD479',
   },
-  highlight: {
-    fontWeight: '700',
+  artwork: {
+    width: 240,
+    height: 240,
+    marginTop: 30,
+    backgroundColor: 'grey',
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: 'white',
+    marginTop: 30,
+  },
+  artistText: {
+    fontSize: 16,
+    fontWeight: '200',
+    color: 'white',
+  },
+  progressContainer: {
+    height: 40,
+    width: 380,
+    marginTop: 25,
+    flexDirection: 'row',
+  },
+  progressLabelContainer: {
+    width: 370,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  progressLabelText: {
+    color: 'white',
+    fontVariant: ['tabular-nums'],
+  },
+  actionRowContainer: {
+    width: '60%',
+    flexDirection: 'row',
+    marginBottom: 100,
+    justifyContent: 'space-between',
+  },
+  primaryActionButton: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#FFD479',
+  },
+  secondaryActionButton: {
+    fontSize: 14,
+    color: '#FFD479',
   },
 });
 
